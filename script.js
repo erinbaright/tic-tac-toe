af5aa9ae77867c882f02b448c6d65fb79ac44704
Erin Baright <erinbaright@Erins-MacBook-Pro.local>
2022-10-04 16:46:17 -0400

    tic tac toe

--- script.js
+++ script.js
@@ -1,119 +1,92 @@
-const tiles = document.querySelectorAll(".tile");
-const playerRed = "Red";
-const playerBlue = "Blue";
-let turn = playerBlue;
+const statusDisplay = document.querySelector(".game-status");
 
-const boardState = Array(tiles.length);
-boardState.fill(null);
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
 
-//Elements
-const strike = document.getElementById("strike");
-const gameOverArea = document.getElementById("game-over-area");
-const gameOverText = document.getElementById("game-over-text");
-const restart = document.getElementById("play-again");
-restart.addEventListener('click', startNewGame);
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `It's a draw.`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
-tiles.forEach((tile) => tile.addEventListener("click", tileClick));
+statusDisplay.innerHTML = currentPlayerTurn();
 
-function setHoverText() {
-  //remove all hover text
-  tiles.forEach((tile) => {
-    tile.classList.remove("red-hover");
-    tile.classList.remove("blue-hover");
-  });
-
-  const hoverClass = `${turn.toLowerCase()}-hover`;
-
-  tiles.forEach((tile) => {
-    if (tile.innerText == "") {
-      tile.classList.add(hoverClass);
-    }
-  });
-}
-
-setHoverText();
-
-function tileClick(event) {
-  if (gameOverArea.classList.contains("visible")) {
-    return;
-  }
-
-  const tile = event.target;
-  const tileNumber = tile.dataset.index;
-  if (tile.innerText != "") {
-    return;
-  }
-
-  if (turn === playerBlue) {
-    tile.innerText = playerBlue;
-    boardState[tileNumber - 1] = playerBlue;
-    turn = playerRed;
-
-  } else {
-    tile.innerText = playerRed;
-    boardState[tileNumber - 1] = playerRed;
-    turn = playerBlue;
-  }
-
-  setHoverText();
-  checkWinner();
-}
-
-function checkWinner() {
-  //Check for a winner
-  for (const winningCombination of winningCombinations) {
-    //Object Destructuring
-    const { combo, strikeClass } = winningCombination;
-    const tileValue1 = boardState[combo[0] - 1];
-    const tileValue2 = boardState[combo[1] - 1];
-    const tileValue3 = boardState[combo[2] - 1];
-
-    if (
-      tileValue1 != null &&
-      tileValue1 === tileValue2 &&
-      tileValue1 === tileValue3
-    ) {
-      strike.classList.add(strikeClass);
-      gameOverScreen(tileValue1);
-      return;
-    }
-  }
-
-  //Check for a draw
-  const allTileFilledIn = boardState.every((tile) => tile !== null);
-  if (allTileFilledIn) {
-    gameOverScreen(null);
-  }
-}
-
-function gameOverScreen(winnerText) {
-  let text = "It's a tie.";
-  if (winnerText != null) {
-    text = `Winner is ${winnerText}!`;
-  }
-  gameOverArea.className = "visible";
-  gameOverText.innerText = text;
-}
-
-function startNewGame() {
-  strike.className = "strike";
-  gameOverArea.className = "hidden";
-  boardState.fill(null);
-  tiles.forEach((tile) => (tile.innerText = ""));
-  turn = playerBlue;
-  setHoverText();
-}
-
-const winningCombinations = [
-  //rows
-  { combo: [1, 2, 3], strikeClass: "strike-row-1" },
-  { combo: [4, 5, 6], strikeClass: "strike-row-2" },
-  { combo: [7, 8, 9], strikeClass: "strike-row-3" },
-  //columns
-  { combo: [1, 4, 7], strikeClass: "strike-column-1" },
-  { combo: [2, 5, 8], strikeClass: "strike-column-2" },
-  { combo: [3, 6, 9], strikeClass: "strike-column-3" },
-  //diagonals
-  { combo: [1, 5, 9], strikeClass: "strike-diagonal-1" },
-  { combo: [3, 5, 7], strikeClass: "strike-diagonal-2" },
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
+
+function handleTilePlayed(clickedTile, clickedTileIndex) {
+  gameState[clickedTileIndex] = currentPlayer;
+  clickedTile.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+  statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleResultValidation() {
+  let roundWon = false;
+  for (let i = 0; i < winningConditions.length; i++) {
+    const winCondition = winningConditions[i];
+    let a = gameState[winCondition[0]];
+    let b = gameState[winCondition[1]];
+    let c = gameState[winCondition[2]]; 
+    console.log(a, b, c);
+    if (a === "" || b === "" || c === "") {
+      continue;
+    }
+    if (a === b && b === c) {
+      roundWon = true;
+      break;
+    }
+  }
+
+  if (roundWon) {
+    statusDisplay.innerHTML = winningMessage();
+    gameActive = false;
+    return;
+  }
+
+  let roundDraw = !gameState.includes("");
+  if (roundDraw) {
+    statusDisplay.innerHTML = drawMessage();
+    gameActive = false;
+    return;
+  }
+
+  handlePlayerChange();
+}
+
+function handleTileClick(event) {
+  const clickedTile = event.target;
+  const clickedTileIndex = parseInt(clickedTile.dataset.index);
+
+  if (gameState[clickedTileIndex] !== "" || !gameActive) {
+    return;
+  }
+
+  handleTilePlayed(clickedTile, clickedTileIndex);
+  handleResultValidation();
+}
+
+function handleRestartGame() {
+  gameActive = true;
+  currentPlayer = "X";
+  gameState = ["", "", "", "", "", "", "", "", ""];
+  statusDisplay.innerHTML = currentPlayerTurn();
+  document.querySelectorAll(".tile").forEach((tile) => (tile.innerHTML = ""));
+}
+
+document
+  .querySelectorAll(".tile")
+  .forEach((tile) => tile.addEventListener("click", handleTileClick));
+document
+  .querySelector(".restart-button")
+  .addEventListener("click", handleRestartGame);
